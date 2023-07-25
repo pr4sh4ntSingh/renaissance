af049a01436408fb75c54955ab0f6f82288de7b9
Prashant Singh <pr4sh4ntSingh@users.noreply.github.com>
2023-07-25 14:46:34 +0530

    Update longest_palindrom.py
    Updated Documentation for longest_palindrom bruteforce implementation.

--- longest_palindrom.py
+++ longest_palindrom.py
@@ -5,17 +5,38 @@ Created on Tue Jul 25 11:08:51 2023
 
 @author: prashantsingh
 """
-""" 89/141
-39/141
-"""
 
 class Solution:
+    """Implementation 1-  BRUTEFORCE, Not Optimal, TLE 
+     ---------------------------------------------------------------------------
+     NOTE- this class has two implementations. Checkout commented section in 
+     longestPalindrome().
+     
+     Algorithm:
+         1. function  is_palindrom checks if the given string is palindrom
+             - Implementation is recursive O(n). (slightly better version could be 
+                using for loop.)
+         2. Create all substring of the given string - O(n2)
+         3. Check all the substrings `get_max_palindrom` for palindrom, return longest.
+    ---------------------------------------------------------------------------
+    Time Complexity : O(n2)
+    Space Complexity : O(n) [if recursion version is used.]
+    """
+    
     def longestPalindrome(self, s: str) -> str:
+        # Check recursively. Passes 39/141 before TLE.
         #i,j = self.check_longest_palindrom(s, i=0, j=len(s)-1)
-        return self.get_max_palindrom(s)
         #return s[i:j+1] 
-        
+
+        # Passes 89/141 cases before TLE.
+        return self.get_max_palindrom(s)
+
+    
     def check_longest_palindrom(self,s,i,j):
+        """ i=0, j= len(s)-1 #needs more analysis. #TLE after 39 cases.
+            check if s[i:j] is palindrom.
+                if not check recusively s[i:j-1] and s[i+1,j] and return max of both.
+        """
         #print(f'checking for {s[i:j+1]} ')
         if self.is_palindrome(s, i, j):
             #print(i,j)
@@ -31,10 +52,12 @@ class Solution:
                 return (i3,i4)
         
     def get_max_palindrom(self,s):
+        """start from (0,0) check
+        """
         max_palindrom_index, max_length = (0,0), 0
         # get all substrings
         for i in range(len(s)):
-            for j in range(len(s)):
+            for j in range(j,len(s)):
                 if self.is_palindrome(s,i,j):
                     if j-i > max_length:
                         max_length = j-i
